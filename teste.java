4a95f7f4a9233cadbbb248a5ffc76ec609b86e50
Danielle <danimiike@gmail.com>
2020-08-12 16:55:36 -0400

    Create teste.java

---
+++ teste.java
@@ -0,0 +1,34 @@
+/**
+ * Program:			teste.java
+ * Date:			Nov 29, 2019
+ * Author:			D. Miike
+ * Description:		 
+ */
+
+import java.util.*;
+
+
+public class teste {
+
+	/**
+	* Method Name:		main
+	* Purpose:		
+	* Accepts:			
+	* Returns:			void
+	*/
+
+	public static void main(String[] args) {
+
+		// Create a Scanner object to read keyboard inputs
+		Scanner input = new Scanner(System.in);
+
+		// Display a program description/title 
+		System.out.println("\n");
+
+		// Add program code here!
+
+		// Close the Scanner object
+		input.close();
+	}
+
+}
